a1ab428acc85f9224e980918b54f4818ceb561db
DESKTOP-EM2NVUS\tooms <99041312@mydavinci.nl>
2017-04-18 13:37:20 +0200

    opdracht 2
    opdracht 2 af

---
+++ deel 1/opdracht2.php
@@ -0,0 +1,21 @@
+<?php
+
+
+	$number = rand(0,10);
+	echo $number . " keer ";
+	$twee = $number * 10;
+	echo $twee;
+?>
+	<br>
+
+<?php  
+
+	
+	if ($number /5) {
+		echo $twee . " dit is deelbaar door 5";
+	}
+
+?>
+		
+
+	
---
+++ deel 1/opdracht3.php
@@ -0,0 +1,39 @@
+<?php 
+$appel = "appel";
+$peer = "peer";
+$spinazie = "spinazie" ;
+$andijvie = "andijvie";	
+$aardappel = "aardappel";	
+	
+	
+
+	switch ($appel) {
+		case 'spinazie':
+			echo "dit is de groente soort spinazie";
+			break;
+		case 'appel':
+			echo "dit is de fruit soort appel";
+			break;
+		case 'peer':
+			echo "dit is de fruit soort peer";
+			break;
+		case 'andijvie':]
+			echo "dit is de groente soort andijvie";
+			break;
+		
+		default:
+			echo "dit is geen groente of fruit";
+			break;
+		
+			
+			
+
+	}
+?>
+
+
+
+
+
+
+ 
