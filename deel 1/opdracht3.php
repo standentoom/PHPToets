272e1dab24c867d456cfdb31bef08033ef04cea3
DESKTOP-EM2NVUS\tooms <99041312@mydavinci.nl>
2017-04-18 13:45:00 +0200

    opdracht 3
    af

--- deel 1/opdracht3.php
+++ deel 1/opdracht3.php
@@ -1,23 +1,19 @@
 <?php 
-$appel = "appel";
-$peer = "peer";
-$spinazie = "spinazie" ;
-$andijvie = "andijvie";	
-$aardappel = "aardappel";	
+$food = array(1 => 'appel', 2 => 'peer', 3 =>'spinazie', 4 =>'andijvie',5 => 'aardappel');
 	
 	
 
-	switch ($appel) {
-		case 'spinazie':
+	switch (1) {
+		case '3':
 			echo "dit is de groente soort spinazie";
 			break;
-		case 'appel':
+		case '1':
 			echo "dit is de fruit soort appel";
 			break;
-		case 'peer':
+		case '2':
 			echo "dit is de fruit soort peer";
 			break;
-		case 'andijvie':]
+		case '4':
 			echo "dit is de groente soort andijvie";
 			break;
 		
